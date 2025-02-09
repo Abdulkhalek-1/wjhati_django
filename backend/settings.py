e5a410649afc4a8629fe7d118a1948c096ad11e7
unknown <nahari771735416@gmail.com>
2025-02-09 10:50:08 -0900

    2/9

--- backend/settings.py
+++ backend/settings.py
@@ -98,6 +98,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    # يمكن إضافة إعدادات أخرى هنا
+}
 
 
 # Internationalization
